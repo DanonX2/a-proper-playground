d2a8024d2bd87e228a56153d592197ee56519b0d
samuel <chensrj2@gmail.com>
2019-02-03 21:58:26 -0600

    Update test1.py

--- phrase_two/test1.py
+++ phrase_two/test1.py
@@ -1,7 +1,30 @@
-from tensorflow.keras import model
+import tensorflow as tf
 from tensorflow.keras import layers
-
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+#generate data
+size = 10
+feature = np.random.random((size,1)) * 100 
+hidden_feature = np.random.random((size,1)) * 100 
+
+target = feature*0.2 + hidden_feature
+
+old_model = tf.keras.Sequential()
+old_model.add(layers.Dense(64, activation='relu'))
+old_model.add(layers.Dense(64, activation='relu'))
+old_model.add(layers.Dense(1, activation='linear'))
+
+old_model.compile(
+    optimizer=tf.train.GradientDescentOptimizer(0.00001),
+    loss='mse',
+    metrics=['mae'])
+
+old_model.fit(feature,target,epochs=100,batch_size=10)
+predict = old_model.predict(feature, batch_size=10)
+plt.scatter(feature,target)
+plt.scatter(feature,predict,c='red')
+plt.show()
