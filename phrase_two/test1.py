2d055241a2d3b15e534892bbb382af9b53cc843c
samuel <chensrj2@gmail.com>
2019-02-05 08:33:03 -0600

    Update test1.py

--- phrase_two/test1.py
+++ phrase_two/test1.py
@@ -14,17 +14,40 @@ hidden_feature = np.random.random((data_size,1))
 target = feature*0.2 + hidden_feature
 #target = feature*feature
 
-old_model = tf.keras.Sequential()
-old_model.add(layers.Dense(50, activation='relu',kernel_initializer='random_uniform',bias_initializer='random_uniform'))
-old_model.add(layers.Dense(1, activation='linear',kernel_initializer='random_uniform',bias_initializer='random_uniform'))
-
+#hyperparameter
 epochs = 5000
 lr = 0.0001
 decay_rate = lr*2 / epochs
 momentum = 0.5
-
-
 optimizer = tf.keras.optimizers.SGD(lr=lr,decay=decay_rate,momentum=momentum)
+
+
+hidden_feature_predictor = tf.keras.Sequential([
+    layers.Dense(50, activation='relu',kernel_initializer='random_uniform',bias_initializer='random_uniform'),
+    layers.Dense(50, activation='relu',kernel_initializer='random_uniform',bias_initializer='random_uniform'),
+    layers.Dense(50, activation='relu',kernel_initializer='random_uniform',bias_initializer='random_uniform'),
+    layers.Dense(1, activation='linear',kernel_initializer='random_uniform',bias_initializer='random_uniform')
+])
+
+hidden_feature_predictor.compile(
+    optimizer = optimizer,
+    loss='mae',
+    metrics=['mae']
+)
+hidden_feature_predictor.fit()
+
+
+
+
+
+
+
+
+
+
+old_model = tf.keras.Sequential()
+old_model.add(layers.Dense(50, activation='relu',kernel_initializer='random_uniform',bias_initializer='random_uniform'))
+old_model.add(layers.Dense(1, activation='linear',kernel_initializer='random_uniform',bias_initializer='random_uniform'))
 
 
 old_model.compile(
